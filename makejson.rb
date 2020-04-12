2440045ddf1aa6ee80595cb2ce3d761ef45fe997
Peter Kappus <peterk@peterkappus.com>
2020-04-12 12:48:34 +0100

    add makejason

---
+++ makejson.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby 
+require 'json'
+
+#build the JSON, stripping the 'public/' prefix from the URLs cuz it'll be served from within the public folder
+json = JSON.generate(Dir.glob("public/media/*.mp3").sort.reverse.map{|u| u.sub("public\/","")})
+
+#write it
+IO.write "public/tracks.json", json
