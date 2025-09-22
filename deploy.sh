caf272fb7159db427415d8048ce527654fc661db
Peter Kappus <62189+peterkappus@users.noreply.github.com>
2025-09-22 11:41:46 +0100

    add link to spotify, fix typos, etc

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,7 @@ source secrets.env
 ./build.sh
 
 #sync 'public' folder to bucket
-docker run -v "$(pwd)"/public:/data --env AWS_ACCESS_KEY_ID=$AWS_ID --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET garland/aws-cli-docker aws s3 sync . s3://piano.peterkappus.com --delete --size-only --acl=public-read --exclude=".git*"
+docker run -v "$(pwd)"/public:/aws --env AWS_ACCESS_KEY_ID=$AWS_ID --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET public.ecr.aws/aws-cli/aws-cli:2.15.56 s3 sync . s3://piano.peterkappus.com --delete --size-only --acl=public-read --exclude=".git*"
 
-docker run -v "$(pwd)"/public:/data --env AWS_ACCESS_KEY_ID=$AWS_ID --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET garland/aws-cli-docker aws cloudfront create-invalidation --distribution-id E1UCHX5XQ0751H --paths /tracks.json /index.html /player.js
+docker run -v "$(pwd)"/public:/aws --env AWS_ACCESS_KEY_ID=$AWS_ID --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET public.ecr.aws/aws-cli/aws-cli:2.15.56 cloudfront create-invalidation --distribution-id E1UCHX5XQ0751H --paths /tracks.json /index.html /player.js
 
-#invlidate json (or index, or player.js)
-#docker run -v "$(pwd)"/public:/data --env AWS_ACCESS_KEY_ID=$AWS_ID --env AWS_SECRET_ACCESS_KEY=$AWS_SECRET garland/aws-cli-docker aws aws cloudfront create-invalidation --distribution-id E1UCHX5XQ0751H --paths "tracks.json" "index.html" "player.js"
--- record.sh
+++ record.sh
@@ -2,14 +2,18 @@
 
 # See: https://gist.github.com/peterkappus/c1b8dfc621077c42019d21cf6f9ab624
 
+echo "Make sure the input is set to your audio interface (not internal microphone).\n\nUNPLUG YOUR LAPTOP POWER SUPPLY WHILE RECORDING :); plug keyboard into inputs 3 and 4 since mic is using in put 1"
 
-threshold="0.5%"
+echo "Ready??? Press ENTER"
+read ready
+
+start_threshold="0.05%"
+end_threshold="0.04%"
 #threshold="0.005%"
 
-echo "Make sure the input is set to your audio interface (not internal microphone).\n\nUNPLUG YOUR POWER SUPPLY WHILE RECORDING :)"
 
 ##open this page to record midi simultaneously:
-#open -na "Google Chrome" --args --incognito "https://midi-recorder.web.app/"
+open -na "Google Chrome" --args --incognito "https://midi-recorder.web.app/"
 
 #file="../all/"$(date +%Y-%m-%dT%H%M%S%Z) 
 file="../all/"$(date +%Y-%m-%dT%H%M) 
@@ -20,18 +24,21 @@ file="../all/"$(date +%Y-%m-%dT%H%M)
 #something bad happened in Apr 2020 and I started getting a buzzzzzz so I've had to change the thresholds to be higher
 #rec -c2 $file.flac silence 1 0:01 0.00008% 1 0:02 0.0003% norm
 
-rec -c2 $file.flac silence 1 0:01 $threshold 1 0:02 $threshold norm
+#rec -c2 $file.flac silence 1 0:01 $threshold 1 0:02 $threshold norm
+
+#NOTE! get channels 3 and 4 since mic is now plugged into channel 1
+rec -c2 $file.flac silence 1 0:01 $start_threshold 1 0:01 $end_threshold remix 3 4 norm
 
 
 sox $file.flac $file.mp3 compand 0.3,1 6:-70,-60,-40 -5 -90 0.2
 
-echo "Any good?"
+echo "Any good? Press 'n' to discard."
 
 read save
 
 if [ "$save" = "n" ]
 then
-  rm $file.mp3 $file.flac
+  rm -v $file.mp3 $file.flac
 fi
 
 #file="../all/"$(date +%Y-%m-%dT%H%M%S%Z) 
